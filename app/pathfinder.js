23942863ccffa83a0d2d45db3d4176d77cecfcb0
Sundowns <tom@smallridge.com.au>
2018-10-11 22:35:29 +0800

    A* implemented and working. Maybe diagonals next?

--- app/pathfinder.js
+++ app/pathfinder.js
@@ -12,9 +12,18 @@ class Cell {
         this.g = 0;
         this.h = 0;
         this.neighbours = [];
+        this.previous = null;
+        this.wall = false;
+
+        if (random(1) < 0.3) {
+            this.wall = true;
+        }
 
         this.show = (colour) => {
             fill(colour);
+            if (this.wall) {
+                fill(0);
+            }
             noStroke();
             rect(this.x*cell_w, this.y*cell_h, cell_w-1, cell_h-1);
         }
@@ -29,8 +38,8 @@ class Cell {
 }
 
 // Globals
-const cols = 10;
-const rows = 10;
+const cols = 30;
+const rows = 30;
 const grid = new Array(cols);
 var cell_w;
 var cell_h;
@@ -40,6 +49,7 @@ var openSet = [];
 var closedSet = []; 
 var start;
 var end;
+var path = [];
 
 function setup() {
     createCanvas(400, 400)
@@ -61,7 +71,8 @@ function setup() {
     }
 
     start = grid[0][0];
-    end = grid[cols-1][rows-1];
+    end = grid[randomIntBetween(Math.floor(cols/2), cols-1)][randomIntBetween(Math.floor(rows/2), rows-1)]
+    end.wall = false;
 
     openSet.push(start);
 
@@ -70,7 +81,6 @@ function setup() {
 
 function draw() { 
     aStarStep();
-    //TODO: 32:39 into the vid https://www.youtube.com/watch?v=aKYlikFAV4k
     background(0);
 
     for (var i = 0; i < cols; i++) {
@@ -80,16 +90,26 @@ function draw() {
     }
 
     closedSet.forEach((cell) => {
-        cell.show(color(255,0,0))
+        cell.show(color(255,165,0));
     });
 
     openSet.forEach((cell) => {
-        cell.show(color(0,255,0))
+        cell.show(color(255,255,0));
     });
+    
+    path.forEach((cell) => {
+        cell.show(color(0,255,0));
+    });
+
+    end.show(color(255,0,255));
 }
 
 const removeFromArray = (array, element) => {
     array.splice(array.lastIndexOf(element), 1); 
+}
+
+const randomIntBetween = (min,max) => {
+    return Math.floor(Math.random()*(max-min+1)+min);
 }
 
 const getCurrentWinner = () => {
@@ -102,20 +122,55 @@ const getCurrentWinner = () => {
     return winner;
 }
 
+const heuristic = (a, b) => {
+    return abs(a.x-b.x) + abs(a.y+b.y);
+}
+
+const evaluatePath = (current) => {
+    path = [];
+    var temp = current;
+    path.push(temp);
+    while (temp.previous) {
+        path.push(temp.previous);
+        temp = temp.previous;
+    }
+}
+
 const aStarStep = () => {
     if (openSet.length > 0) {
         var winner = getCurrentWinner();
 
         var current = openSet[winner];
-
+        evaluatePath(current);
         if (current === end) {
-            console.log("DONE!")
+            noLoop();
+            console.log("DONE!");
         }
 
         removeFromArray(openSet, current);
         closedSet.push(current);
 
+        current.neighbours.forEach((neighbour) => {
+            if (!closedSet.includes(neighbour) && !neighbour.wall) {
+                var tempG = current.g + 1;
+
+                if (openSet.includes(neighbour)) {
+                    if (tempG < neighbour.g) {
+                        neighbour.g = tempG;
+                    }
+                } else {
+                    neighbour.g = tempG;
+                    openSet.push(neighbour);
+                }
+
+                neighbour.h = heuristic(neighbour, end);
+                neighbour.f = neighbour.g + neighbour.h;
+                neighbour.previous = current;
+            }
+        })
+
     } else {
+        console.log("No Solution :(!")
         // no solution
     }
 }
